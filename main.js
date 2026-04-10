cb4b61a8bb6d16aa0768ddaf930ac875c68bab91
prathiprathiba <prathiprathiba2006@gmail.com>
2026-04-10 20:12:27 +0530

    route structure

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ const port = 6969;
 app.get("/", (req, res) => {
     res.json({ msg: "hello friends" });
 });
+// CRUD functionality of movies
+//C - create
+app.get("/movies", () => {
+
+})
+
+//R -Read 
+app.post("/movies", (req, res) => {
+
+})
+
+//U -update 
+app.put("/movies/:id", (req, res) => {
+
+})
+
+//D -delete
+app.delete("/movies/:id", (req, res) => {
+
+})
+
+
+
+
 app.listen(port, () => {
     console.log(`server running on the http://localhost:${port}`);
 
